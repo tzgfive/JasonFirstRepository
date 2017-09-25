04025c929610c2ad108d0b6ccecafe1780a7f4e5
tzgfive <32115291+tzgfive@users.noreply.github.com>
2017-09-25 11:26:59 -0400

    Create mathquiz.js

---
+++ mathquiz.js
@@ -0,0 +1,6 @@
+function moreStuff(){
+document.getElementById ("solution").innerHTML=
+"<P>A paraph</p><h2> A Heading<h2>"+
+"<p>Another paragraph "+
+"with a <a href='http://www.cvuhs.org'>link</a></p>"
+}
