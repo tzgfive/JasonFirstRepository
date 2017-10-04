567534a3b12acaa4ab6f3d1b86c1942f9c2f5d34
tzgfive <32115291+tzgfive@users.noreply.github.com>
2017-10-04 11:08:35 -0400

    Update mathquiz.js

--- mathquiz.js
+++ mathquiz.js
@@ -1,6 +1,6 @@
 function moreStuff(){
 document.getElementById ("solution").innerHTML=
 "<P>A paraph</p><h2> A Heading<h2>"+
-"<p>Another paragraph "+
+"<p>Another paragraph</p> "+
 "with a <a href='http://www.cvuhs.org'>link</a></p>"
 }
